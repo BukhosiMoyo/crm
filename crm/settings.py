6ddfc046e60d8e209d3864a8a6c0e3f3c9ab7312
Bukhosi Moyo <bukhosizimcode@gmail.com>
2020-11-12 13:22:30 +0200

    init

--- crm/settings.py
+++ crm/settings.py
@@ -138,5 +138,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'symaxxagency@gmail.com'
-EMAIL_HOST_PASSWORD = 'Youmaxxcode1!'
+EMAIL_HOST_USER = 'youremail@gmail.com'
+EMAIL_HOST_PASSWORD = 'Yourpassword1!'
